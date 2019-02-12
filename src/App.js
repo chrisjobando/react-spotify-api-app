870a5b3f61e858d87007e7ae90e23c539d5705c5
Christopher Obando <chrisjobando@gmail.com>
2019-02-12 16:02:50 +0100

    Dynamic listing of test data

--- src/App.js
+++ src/App.js
@@ -82,14 +82,15 @@ class Filter extends Component {
 
 class Playlist extends Component {
   render() {
+    let playlist = this.props.playlist;
     return(
       <div style={{...defaultStyle, display: 'inline-block', width: '25%'}}>
         <img/>
-        <h3>Playlist Name</h3>
+        <h3>{playlist.name}</h3>
         <ul>
-          <li>Song 1</li>
-          <li>Song 2</li>
-          <li>Song 3</li>
+          {playlist.songs.map(song =>
+            <li>{song.name}</li>
+          )}
         </ul>
       </div>
     );
@@ -119,10 +120,9 @@ class App extends Component {
             <HoursCounter playlists=
             {this.state.serverData.user.playlists} />
             <Filter/>
-            <Playlist/>
-            <Playlist/>
-            <Playlist/>
-            <Playlist/>
+            {this.state.serverData.user.playlists.map(playlist =>
+              <Playlist playlist={playlist}/>
+            )}
           </div> : <h1 style={defaultStyle}>Loading...</h1>
         }
       </div>
