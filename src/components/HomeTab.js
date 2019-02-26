6846cbf7d977b6a4b5eaeba23c8b29f66c8b528b
Christopher Obando <chrisjobando@gmail.com>
2019-02-27 00:17:10 +0100

    Hometab updates (changed welcome text)

--- src/components/HomeTab.js
+++ src/components/HomeTab.js
@@ -21,12 +21,23 @@ class HomeTab extends Component {
                     <PlaylistCounter numPlaylists={this.props.state.playlists.length} />
                 </header>
                 <h1>Welcome to My Spotify Web App!</h1>
-                <h2>This is in its early stages, but feel free to try it out!</h2>
                 <br/>
-                <h2 className="links">Made by: Christopher Obando</h2>
-                <h2 className="links"><a href="https://open.spotify.com/user/chrisjobando"
+                <h2 className="head-h2">This is a personal project I have been working on for the
+                    past couple of weeks, to learn more about ReactJS app development, get
+                    practice working with APIs, and to improve my software design skills!</h2>
+                <br/>
+                <h2 className="head-h2">This is currently in a working Beta version, try playing
+                    music on your preferred Spotify app to see how this app pairs with your
+                    device for live controls and updates!
+                </h2>
+                <br/>
+                <h2 className="head-h2">Made by: Christopher Obando</h2>
+                <br/>
+                <h2 className="head-h2"><a href="https://open.spotify.com/user/chrisjobando"
                 target="_blank" rel="noopener noreferrer">Follow My Spotify!</a></h2>
-                <h2 className="links"><a href="https://github.com/chrisjobando/spotify-app"
+                <h2 className="head-h2"><a href="https://github.com/chrisjobando"
+                target="_blank" rel="noopener noreferrer">Follow My Github!</a></h2>
+                <h2 className="head-h2"><a href="https://github.com/chrisjobando/spotify-app"
                 target="_blank" rel="noopener noreferrer">Link To Source Code</a></h2>
                 <br/>
                 <h1>Recently Played</h1>
