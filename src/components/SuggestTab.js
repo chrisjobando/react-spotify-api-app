8c5587cb6654f006284c1b97bb0c31068e72dd36
Christopher Obando <chrisjobando@gmail.com>
2019-02-28 11:20:22 +0100

    Removed outline on click of buttons

--- src/components/SuggestTab.js
+++ src/components/SuggestTab.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 
-
 class SuggestTab extends Component {
     componentDidMount() {
         window.scrollTo(0,0);
@@ -10,7 +9,14 @@ class SuggestTab extends Component {
         return(
             <div style={{minHeight: '50vh'}}>
                 <h1>Recommendations</h1>
-                <h2>Coming Soon...</h2>
+                <br/>
+                <h2>Based on your Recent Mood</h2>
+                <br/>
+                <h2>Based on your Listening Habits this Month</h2>
+                <br/>
+                <h2>Based on your Listening Habits for the Past 6 Months</h2>
+                <br/>
+                <h2>Based on your All-Time Favorites</h2>
             </div>
         );
     }
